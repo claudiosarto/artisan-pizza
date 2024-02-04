cfdfd36d6aa0c02f6df86609b466fb73d26beb81
Claudio Sarto <claudiosarto@gmail.com>
2024-02-04 17:39:55 +0000

    feat: add initial welcome function and define main

--- run.py
+++ run.py
@@ -15,15 +15,34 @@ SHEET = GSPREAD_CLIENT.open('artisan_pizza')
 
 menu = SHEET.worksheet('menu')
 
-
-
 # Define empty menu dictionary
 menu_dictionary = {}
 
 # Add Header as key and remaining items as list of values
+"""
 for column in range(menu.col_count):
     #print(menu.col_values(column+1)[0]) #key value
     #print(menu.col_values(column+1)[1:]) #key list
-    menu_dictionary[menu.col_values(column+1)[0]] = menu.col_values(column+1)[1:]
+    menu_dictionary[menu.col_values(column+1)[0].lower()] = menu.col_values(column+1)[1:]
+"""
+#pprint.pp(menu_dictionary)
 
 
+
+def welcome():
+    """
+    Welcome function to gather Customer Name
+    """
+    print("Welcome to Artisan-Pizza - Where Artisan Craftsmanship Meets Your Cravings!")
+    while True:
+        customername = input("Please enter your name: \n")
+        if customername.isalpha():
+            break
+        else:
+            print("\nPlease check your name, only characters [A-Z] are accepted")
+    return customername
+
+def main():
+    customer=welcome()
+
+main()
